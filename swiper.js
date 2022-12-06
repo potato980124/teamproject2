862a969b34e29be3f6347e278ae6c49f6e553c5b
sewon0076 <119268025+sewon0076@users.noreply.github.com>
2022-12-06 11:32:00 +0900

    Add files via upload

--- swiper.js
+++ swiper.js
@@ -1,23 +1,3 @@
-// var swiper = new Swiper(".swiper", {
-//     slidesPerView: 3,
-//     centeredSlides: true,
-//     loop: true,
-//     spaceBetween: 20,
-//     pagination: {
-//         el: ".swiper-pagination",
-//         clickable: true,
-//         renderBullet: function (index, className) {
-//             return '<span class="' + className + '">' + (index + 1) + "</span>";
-//         },
-//     },
-//     navigation: {
-//         nextEl: ".swiper-button-next",
-//         prevEl: ".swiper-button-prev",
-//     },
-// });
-// if data-swiper-slide-index==0{
-
-// }
 const swiper = new Swiper(".swiper", {
     // Optional parameters
     // effect: "coverflow",
@@ -54,3 +34,81 @@ const swiper = new Swiper(".swiper", {
     el: '.swiper-scrollbar',
   },*/
 });
+
+// if data-swiper-slide-index==0{
+
+// // }
+// let vw = window.innerWidth;
+// if (vw > 800 && vw < 1920) {
+//     const swiper = new Swiper(".swiper", {
+//         // Optional parameters
+//         // effect: "coverflow",
+//         loop: true,
+//         autoHeight: false,
+//         centeredSlides: true,
+//         slidesPerView: 3,
+//         spaceBetween: 0,
+//         // Responsive breakpoints
+//         breakpoints: {
+//             640: {
+//                 slidesPerView: 3,
+//                 spaceBetween: 0,
+//             },
+//             992: {
+//                 slidesPerView: 3,
+//                 spaceBetween: 0,
+//             },
+//         },
+
+//         // If we need pagination
+//         pagination: {
+//             el: ".swiper-pagination",
+//         },
+
+//         // Navigation arrows
+//         navigation: {
+//             nextEl: ".swiper-button-next",
+//             prevEl: ".swiper-button-prev",
+//         },
+
+//         // And if we need scrollbar
+//         /*scrollbar: {
+//     el: '.swiper-scrollbar',
+//   },*/
+//     });
+// }
+// } else if (vw < 800) {
+//     console.log("now" + vw);
+//     const swiper = new Swiper(".swiper", {
+//         // Optional parameters
+//         // effect: "coverflow",
+//         loop: true,
+//         autoHeight: false,
+//         centeredSlides: true,
+//         slidesPerView: 2,
+//         spaceBetween: 0,
+//         // Responsive breakpoints
+//         // breakpoints: {
+//         //     640: {
+//         //         slidesPerView: 3,
+//         //         spaceBetween: 0,
+//         //     },
+//         //     992: {
+//         //         slidesPerView: 3,
+//         //         spaceBetween: 0,
+//         //     },
+//         // },
+
+//         // If we need pagination
+//         // pagination: {
+//         //     el: ".swiper-pagination",
+//         // },
+
+//         // Navigation arrows
+
+//         // And if we need scrollbar
+//         /*scrollbar: {
+//     el: '.swiper-scrollbar',
+//   },*/
+//     });
+// }
