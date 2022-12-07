0cf3ba664df5c47b48cf2a2e5d2de04fdaa5cfda
Yuju <ujusunrise@gmail.com>
2022-12-07 18:45:50 +0900

    12-07 이유주 작업 내역 업데이트

---
+++ js/swiper.js
@@ -0,0 +1,54 @@
+var swiper = new Swiper(".swiper", {
+    // Optional parameters
+    // effect: "coverflow",
+    loop: true,
+    autoHeight: false,
+    centeredSlides: true,
+    slidesPerView: 3,
+    spaceBetween: 0,
+    // Responsive breakpoints
+    breakpoints: {
+        640: {
+            slidesPerView: 3,
+            spaceBetween: 0,
+        },
+        992: {
+            slidesPerView: 3,
+            spaceBetween: 0,
+        },
+    },
+
+    // If we need pagination
+    pagination: {
+        el: ".swiper-pagination",
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    // And if we need scrollbar
+    /*scrollbar: {
+    el: '.swiper-scrollbar',
+  },*/
+});
+
+// <!-- Initialize Swiper -->
+var swiper = new Swiper(".news_mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction"
+    },
+    navigation: {
+      nextEl: ".slide_next",
+      prevEl: ".slide_prev",
+    },
+  });
