0e2efb077b842c2e08d01e2d04d55f41bd3eda3e
sewon <kimsewon0076@gmail.com>
2022-12-12 13:11:03 +0900

    server connection

---
+++ routes/route.js
@@ -0,0 +1,35 @@
+//page 연결이 쉬워지도록
+//
+const express = require("express");
+const router = express.Router();
+const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
+const db = require("./../db.js");
+
+router.get("/", (req, res) => {
+    res.render("main");
+});
+router.get("/committee", (req, res) => {
+    res.render("committee");
+});
+router.get("/festival", (req, res) => {
+    res.render("festival");
+});
+router.get("/event", (req, res) => {
+    res.render("event");
+});
+router.get("/notice_list", (req, res) => {
+    res.render("notice_list");
+});
+router.get("/gallery", (req, res) => {
+    res.render("gallery");
+});
+router.get("/login", (req, res) => {
+    res.render("login");
+});
+router.get("/join", (req, res) => {
+    res.render("join");
+});
+
+module.exports = router;
