a8c76cbc661a1b4607106b615a3fc9da1d7e3d0e
potato990124 <potato980124@gmail.com>
2022-12-22 10:45:01 +0900

    이벤트 페이지 db 연결 및 지도 api 연결

---
+++ public/js/festivalapi.js
@@ -0,0 +1,17 @@
+//축제 소개 페이지
+let container = document.querySelector('.loca_map_bg');
+let options ={
+    center: new kakao.maps.LatLng(35.100837335972834, 129.03241036354348),
+    level : 2
+}
+
+let map = new kakao.maps.Map(container, options);
+
+let markerPosition  = new kakao.maps.LatLng(35.100837335972834, 129.03241036354348);
+let marker = new kakao.maps.Marker({
+    position: markerPosition
+});
+marker.setMap(map);
+
+
+
--- routes/route.js
+++ routes/route.js
@@ -6,6 +6,20 @@ const multer = require("multer");
 const fs = require("fs");
 const path = require("path");
 const db = require("./../db.js");
+const upload = multer({
+    storage: multer.diskStorage({
+        destination(req, file, done) {
+            done(null, '../public/uploads/');
+        },
+        filename(req, file, done) {
+            const ext = path.extname(file.originalname); //파일의 확장자
+            done(null, path.basename(file.originalname, ext) + Date.now() + ext); //파일명 + 날짜 + 확장자명
+        }
+    }),
+    limits: {
+        fileSize: 1024 * 1024 * 9
+    } //2메가까지 업로드 가능
+})
 //notice=====================================
 router.get("/", (req, res) => {
     res.render("main");
@@ -17,7 +31,10 @@ router.get("/", (req, res) => {
 // });
 router.get("/notice_list", (req, res) => {
     db.getNotice((rows1, rows2) => {
-        res.render("notice_list", { rows1: rows1, rows2: rows2 }); //ejs의 rows를 받아서 rows라는 이름으로 보낸다
+        res.render("notice_list", {
+            rows1: rows1,
+            rows2: rows2
+        }); //ejs의 rows를 받아서 rows라는 이름으로 보낸다
     });
 });
 router.get("/notice_write", (req, res) => {
@@ -38,7 +55,9 @@ router.post("/w_notice", (req, res) => {
 router.get("/notice_detail", (req, res) => {
     let id = req.query.id;
     db.getNoticeByid(id, (row) => {
-        res.render("notice_content", { row: row[0] }); //테이블의 한 행만 보내줄거기 때문에
+        res.render("notice_content", {
+            row: row[0]
+        }); //테이블의 한 행만 보내줄거기 때문에
     });
 });
 //notice====2222222=====================================
@@ -67,13 +86,34 @@ router.get("/festival", (req, res) => {
 });
 //---이벤트 페이지---
 router.get("/event", (req, res) => {
-    res.render("event");
+    db.getEvent((havors,flowers,fires,rocks,seas,citys,rings) => {
+        res.render("event", {
+            havors: havors,
+            flowers: flowers,
+            fires: fires,
+            rocks:rocks,
+            seas:seas,
+            citys:citys,
+            rings:rings
+        });
+    })
 });
 // 이벤트 등록 페이지
-router.get('/eventwrite',(req,res)=>{
+router.get('/eventwrite', (req, res) => {
     res.render("event_write");
 })
-
+router.post('/w_event', upload.single('eventimg'), (req, res) => {
+    let param = JSON.parse(JSON.stringify(req.body));
+    let writer = param['name'];
+    let pw = param['password'];
+    let category = param['category'];
+    let title = param['title'];
+    let content = param['content'];
+    let eventimg = 'uploads/' + req.file.filename;
+    db.insertIntoEvent(writer, pw, category, title, content, eventimg, () => {
+        res.redirect('/event');
+    })
+})
 
 //---갤러리---
 router.get("/gallery", (req, res) => {
@@ -96,7 +136,9 @@ router.post("/loginCheck", (req, res) => {
 });
 router.get("/join", (req, res) => {
     db.getJointable((ids) => {
-        res.render("join", { ids: ids });
+        res.render("join", {
+            ids: ids
+        });
     });
 });
 router.post("/joininfo", (req, res) => {
