6799490548240e80aeddea52b358a12a14777c1c
potato990124 <potato980124@gmail.com>
2022-12-23 09:42:42 +0900

    헤더수정

--- public/js/header.js
+++ public/js/header.js
@@ -7,6 +7,7 @@ gnbMenu.forEach((e,index)=>{
         setTimeout(()=>{
           lnb[index].style.display = 'flex';
           gnbBg.style.height = '70px';
+          // gnbBg.style.borderBottom = '1px solid rgba()';
         },300);
         setTimeout(()=>{
           lnb[index].style.opacity = '1';
@@ -16,6 +17,7 @@ gnbMenu.forEach((e,index)=>{
           setTimeout(()=>{
               lnb[index].style.display = 'none';
               gnbBg.style.height = '0';
+              // gnbBg.style.borderBottom = '0';
           },300);
           setTimeout(()=>{
             lnb[index].style.opacity = '0';
